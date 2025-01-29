19d74b9de14a05bd638e181c101671734473402f
merisir573 <aikosstory@gmail.com>
2025-01-29 20:33:22 +0300

    Updated API routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller('auth/v1')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
